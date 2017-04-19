84d239749e4438779ee20e159a1a2f7390e34aa2
Samy <Samy>
2017-04-19 16:18:17 +0200

    test

--- YTconvert/app/src/main/java/com/example/selim_000/ytconvert/MainActivity.java
+++ YTconvert/app/src/main/java/com/example/selim_000/ytconvert/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.selim_000.ytconvert;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String URL_MUSIC_SEARCH = "//www.youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v=i62Zjga8JOM";
@@ -11,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         DownloadJsonTask jsonTask = new DownloadJsonTask();
         jsonTask.execute(URL_MUSIC_SEARCH);
+
+        setContentView(R.layout.activity_main);
+
+        final Context context = this;
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http:\\/\\/www.youtubeinmp3.com\\/download\\/get\\/?i=iQrVWoRvs4SSS80nnmp5JB7YyOmQCiXt&e=27"));
+                startActivity(intent);
+            }
+        });
     }
 }
