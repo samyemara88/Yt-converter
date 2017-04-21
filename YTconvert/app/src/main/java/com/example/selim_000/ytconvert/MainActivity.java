9e12336707f512cbd2842a5ebfdc647a9ce71096
Samy <Samy>
2017-04-21 15:41:38 +0200

    hh

--- YTconvert/app/src/main/java/com/example/selim_000/ytconvert/MainActivity.java
+++ YTconvert/app/src/main/java/com/example/selim_000/ytconvert/MainActivity.java
@@ -11,22 +11,34 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText text_url;
+    private Button buttonDownload;
     private  final String URL_MUSIC_SEARCH = "https://www.youtubeinmp3.com/fetch/?format=JSON&video=";
     @Override
 
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            setContentView(R.layout.activity_main);
+            recreate();
+
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         checkPermission();
 
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         text_url = (EditText)findViewById (R.id.editText);
 
 
@@ -46,12 +58,31 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+
+
+
+
                 String video_url =  text_url.getText().toString() ;
                 String last_url = URL_MUSIC_SEARCH + video_url ;
                 jsonTask.execute(last_url);
+                Context context = getApplicationContext();
+                CharSequence text = "Votre musique est en cour de téléchargement";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        buttonDownload = (Button) findViewById(R.id.button2);
+        buttonDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.downloads);
             }
         });
     }
+
+
 
     private void checkPermission(){
 
@@ -74,4 +105,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
+
 }
